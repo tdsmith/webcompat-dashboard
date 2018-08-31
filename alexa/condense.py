cd5b1bae43c70fbe0851565762988201c7d48896
Tim Smith <tdsmith@mozilla.com>
2018-08-31 14:46:45 -0700

    Add Alexa world rank summary

---
+++ alexa/condense.py
@@ -0,0 +1,31 @@
+import json
+import xml.etree.ElementTree as ET
+
+flags = {
+    "china": "🇨🇳",
+    "france": "🇫🇷",
+    "germany": "🇩🇪",
+    "global": "🌎",
+    "usa": "🇺🇸",
+    "russia": "🇷🇺",
+}
+
+NS = {"ats": "http://ats.amazonaws.com/doc/2005-07-11"}
+
+ranks = {}
+
+for country in flags:
+    tree = ET.parse(f"{country}.xml")
+    sites = tree.findall(".//ats:TopSites/ats:Country/ats:Sites/*", NS)
+    for site in sites:
+        domain = site.find("ats:DataUrl", NS).text
+        rank = site.find("ats:Country/ats:Rank", NS).text
+        reach = site.find("ats:Country/ats:Reach/ats:PerMillion", NS).text
+        ranks.setdefault(domain, {})[country] = (int(rank), float(reach))
+
+condensed = {}
+for domain in ranks:
+    highest = sorted(ranks[domain].items(), key=lambda x: x[1][1], reverse=True)[0]
+    condensed[domain] = "{} #{}".format(flags[highest[0]], highest[1][0])
+
+print(json.dumps(condensed))
